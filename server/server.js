5be53688359179986e1fd4d89fb8075684e87619
Ugur <ugur@localhost.localdomain>
2018-11-21 18:58:09 +0300

    message

--- server/server.js
+++ server/server.js
@@ -1,14 +1,37 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname,"../public");
 const port = process.env.PORT || 3000;
 
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
 
 app.use(express.static(publicPath));
 
-app.listen(port,() => {
+io.on("connection",(socket) => {
+    console.log("New user connected");
+
+ 
+    socket.on("createMessage",(newMessage) => {
+        console.log("Create message",newMessage);
+        io.emit("newMessage",{
+            from:newMessage.from,
+            text:newMessage.text,
+            createdAt:new Date().getTime()
+        });
+    });
+    
+    socket.on("disconnect",() => {
+        console.log("User was disconnected");
+    });
+});
+
+server.listen(port,() => {
     console.log("Server is running on port ",port);
 });
