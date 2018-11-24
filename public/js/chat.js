7d1a62f14656c1c0c2d39480a3de00d02e5b30f3
Ugur <ugur@localhost.localdomain>
2018-11-24 18:57:07 +0300

    last commit

---
+++ public/js/chat.js
@@ -0,0 +1,107 @@
+var socket = io();
+
+function scrollToBottom(){
+    var messages = $("#messages");
+    var newMessage = messages.children("li:last-child");
+
+    var clientHeight = messages.prop("clientHeight");
+    var scrollTop = messages.prop("scrollTop");
+    var scrollHeight = messages.prop("scrollHeight");
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
+
+socket.on("connect",function() {       //BAĞLANTI SAĞLANDIĞINDA 
+    var params = $.deparam(window.location.search);   //gelen parametreleri al 
+
+    socket.emit("join",params,function (err) {    //Join emitini yak
+        if(err) {
+            alert(err);
+            window.location.href = "/";
+        } else {
+            console.log("No error");
+        }
+    });
+});
+
+socket.on("disconnect",function() {
+    console.log("Discnnect from server");
+});
+
+
+socket.on("updateUserList",function(users) {
+    var ol = $("<ol></ol>");
+    
+    users.forEach(function (user) {
+        ol.append($("<li></li>").text(user));
+    });
+
+    $("#users").html(ol);
+});
+
+
+socket.on("newMessage",function(message){   //newMessage tagiyle server.js te oluşturulmuş yeni mesajımızı alıp forma ekledik.
+    var formattedTime = moment(message.createdAt).format("h:mm a");
+    var template = $("#message-template").html();
+   var html = Mustache.render(template,{
+       text:message.text,
+       from:message.from,
+       createdAt:formattedTime
+   });
+
+   $("#messages").append(html);
+   scrollToBottom();
+   
+   
+   // var li = $("<li></li>");
+   // li.text(message.from+" "+formattedTime+": "+message.text);
+   // $("#messages").append(li);
+});
+
+socket.on("newLocationMessage",function(message){    //AŞAĞIDA BUTONA BASILINCA EMİT EDİP YENİ MESAJ OLUŞTURUYORUZ VE SONRA TEKRAR SERVER.JS TE newLOcationMessageyi emit edip bunu yakıyoruz.
+    var formattedTime = moment(message.createdAt).format("h:mm a");
+    var template = $("#location-message-template").html();
+    var html = Mustache.render(template,{
+        from:message.from,
+        createdAt:formattedTime,
+        url:message.url
+    });
+    $("#messages").append(html);
+    scrollToBottom();
+});
+
+$("#message-form").on("submit",function(e) {      //JQUERY İLE FORMU SEÇİP EMİT İLE CREATEMESSAGE KULLANARAK GELEN MESAJI ALDIK.
+    e.preventDefault();
+
+    var messageTextbox = $("[name=message]");
+
+    socket.emit("createMessage",{    //CREATE MESSAGE TAGI KULLANILARAK $NAME=MESSAGE İLE GELEN MESAJ ALINIP YENİ BİR MESAJ OLUŞTURULDU.
+        text:messageTextbox.val()
+    },function(){
+        messageTextbox.val("")
+    });
+});
+
+
+var locationButton = $("#send-location");     //LOCATİON BUTONA TIKLADIĞINDA  
+locationButton.on("click",function() {      // yanıyor,
+    if(!navigator.geolocation){               // EĞER DESTEKLEMİYORSA OLMUYOR
+        return alert("Geolocation not supported your browser");
+    }
+
+    locationButton.attr("disabled","disabled").text("Sending location...");    //BUTONA BASILINCA DİSABLED YAPTIK.
+    navigator.geolocation.getCurrentPosition(function(position){     //DAHA SONRA BİZİM POZİSYONUMUZU ALIP LOCATİON MESSAGE EMİTLEDİĞİNDE TEKRAR GÖRÜNÜR OLUYOR.
+        locationButton.removeAttr("disabled").text("Send Location");
+        socket.emit("createLocationMessage",{     // DEĞERLERİMİZİ EMİT ETTİK.
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude
+        });
+    },function(){
+        alert("Unable to fetch location.");
+    });
+});
--- server/server.js
+++ server/server.js
@@ -3,32 +3,67 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
+const {isRealString} = require("./utils/validation");
+const {Users} = require("./utils/users");
+const {generateMessage,generateLocationMessage} = require("./utils/message");
 const publicPath = path.join(__dirname,"../public");
 const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 
 
 app.use(express.static(publicPath));
 
-io.on("connection",(socket) => {
+io.on("connection",(socket) => {   //CONNECTİON DURUMUNDA SOCKETİMİZLE EMİT VE ON İŞLEMLERİ YAPACAĞIZ.
     console.log("New user connected");
 
+    
  
-    socket.on("createMessage",(newMessage) => {
-        console.log("Create message",newMessage);
-        io.emit("newMessage",{
-            from:newMessage.from,
-            text:newMessage.text,
-            createdAt:new Date().getTime()
-        });
+    socket.on("join",(params,callback) => {   //CHat.js te ki johin emitiyle burayı yak
+        if(!isRealString(params.name) || !isRealString(params.room)){  // Gerçek string değilse oda adı veya name hata callbacki yolla ve girme
+            return callback("Name and room name are required");
+        }
+       
+        socket.join(params.room);  // Diğer durumda oda adıyla join ol.
+        users.removeUser(socket.id);
+        users.addUser(socket.id,params.name,params.room); 
+
+        io.to(params.room).emit("updateUserList",users.getUserList(params.room));
+
+        socket.emit("newMessage",generateMessage("Admin","Welcome to the chat app"));  //NEW MESSAGE TAGİNİ EMİT EDİP GENERATE MESSAGE İLE MESAJ OLUŞTURDUK.
+        socket.broadcast.to(params.room).emit("newMessage",generateMessage("Admin",params.name+" has connected!"));   //YENİ BİR KULLANICI GELİNCE ESKİ KULLANICILARA BİLGİ GİDİYOR.
+        
+        callback();
+    });
+
+    socket.on("createMessage",(message,callback) => {      //CreateMessage kullanılarak gelen mesajı newMessage ile emit ettik.
+        var user = users.getUser(socket.id);
+
+        if(user && isRealString(message.text)) {
+            io.to(user.room).emit("newMessage",generateMessage(user.name,message.text));
+        }
+        callback();
+    });
+
+    socket.on("createLocationMessage",(coords) => {    //İNDEX.JS TE EMİT ETTİĞİMİZ CREATELOCATİONMESSAGEYİ burda alıyoruz ve 
+        var user = users.getUser(socket.id);
+        if(user) {
+            io.to(user.room).emit("newLocationMessage",generateLocationMessage(user.name,coords.latitude,coords.longitude));   //newLocationMessage olarak tekrar emit ediyoruz.
+        }
+
     });
     
-    socket.on("disconnect",() => {
-        console.log("User was disconnected");
+    socket.on("disconnect",() => {    //DİSCONNECT OLUNDUĞU ZAMAN
+        var user = users.removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit("updateUserList",users.getUserList(user.room));
+            io.to(user.room).emit("newMessage",generateMessage("Admin",user.name+" has left!"));
+        }
     });
 });
 
